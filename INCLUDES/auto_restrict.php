d246d3eec78ce70ba5c903790e2042582d7027b0
Bronco <bronco@warriordudimanche.net>
2015-09-02 14:05:00 +0200

    Major security update

--- INCLUDES/auto_restrict.php
+++ INCLUDES/auto_restrict.php
@@ -1,9 +1,5 @@
 <?php
 	/**
-	* BoZoN admin only protection:
-	* part of auto_restrict lib 
-	* @author: Bronco (bronco@warriordudimanche.net)
-	*
 	 * auto_restrict
 	 * @author bronco@warriordudimanche.com / www.warriordudimanche.net
 	 * @copyright open source and free to adapt (keep me aware !)
